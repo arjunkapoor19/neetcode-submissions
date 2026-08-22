140cc2bce0f8125eed9841ba81c01887315cccd5
Arjun Kapoor <52744007+arjunkapoor19@users.noreply.github.com>
2026-08-22 18:58:57 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashmap = set()
+
+        for x in nums:
+            
+            if x in hashmap:
+                return True
+            hashmap.add(x)
+        return False
+            
+         
